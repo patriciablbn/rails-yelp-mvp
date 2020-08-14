0aa9fd20cedfa81a6c2fbb4af6f02c84b6a14a37
Patricia BELLE BONO <patriciablbn@gmail.com>
2020-08-14 11:42:11 +0200

    yelp mvp 1st part

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+omg = { name: "你好", address: "1 rue Oberkampf", category: "chinese" }
+buongiorno =  { name: "Buongiorno", address: "2 rue Oberkampf", category: "italian" }
+olala = { name: "こんにちは", address: "3 rue Oberkampf", category: "japanese" }
+bonjour = { name: "Bonjour", address: "4 rue Oberkampf", category: "french" }
+goedemorgen = { name: "Goedemorgen", address: "5 rue Oberkampf", category: "belgian" }
+
+[omg, buongiorno, olala, bonjour, goedemorgen].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
